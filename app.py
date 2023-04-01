4c8cafadd5d5cfd14cf55c547f3bdf5047300a52
zubckova <118109587+zubckova@users.noreply.github.com>
2023-04-01 21:28:30 +0300

    Поправлены пути, добавлена возможность взятия изображения из кеша

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+import base64
 import requests
 from db import Address, Code
 from flask import Flask, render_template
+from PIL import Image
+from io import BytesIO
+from io import StringIO
+from pymemcache.client import base
+
+client = base.Client(('localhost', 11211))
 
 def request(address):
     try:
@@ -11,20 +18,47 @@ def request(address):
 
 def detect_work_addresses(address):
 
+    print(address)
+
     protocols = [
         "https:", "http:"
     ]
 
     addresses = []
 
-    for protocol in protocols:
-        if request(protocol+"//"+address): addresses.append(protocol+"//"+address)
-
+    if "https://" in address or "http://" in address: 
+        addresses.append(address)
+    else:
+        for protocol in protocols:
+            test_address = protocol+"//"+address
+            if request(test_address): 
+                addresses.append(test_address)
+    print(addresses)
     return addresses
 
 def get_cat_by_status(status):
     response = requests.get(f'https://http.cat/{status}')
     return response.content
+
+def get_image_by_status(status, format="string"):
+    response = requests.get(f'https://http.cat/{status}')
+    img = Image.open(BytesIO(response.content))
+    output_buffer = BytesIO()
+    img.save(output_buffer, format='JPEG')
+    binary_data = output_buffer.getvalue()
+    base64_data = base64.b64encode(binary_data)
+    return base64_data
+
+def check_memory(status):
+    if client.get(f'{status}'):
+        print("Image from cache.")
+        return str(client.get(f'{status}'), 'utf-8')
+    else: 
+        image_binary = get_image_by_status(status)
+        client.set(f'{status}', image_binary)
+        print("Image is downloaded.")
+        return str(image_binary, 'utf-8')
+    
 
 def save_info_about_request(status, address):
 
@@ -40,13 +74,15 @@ def save_info_about_request(status, address):
 
 app = Flask(__name__)
 
-@app.route('/<address>')
+@app.route('/<path:address>')
 def show_status(address):
     address = detect_work_addresses(address)
+
     try:
         response = requests.get(address[0])
         status = response.status_code
+        image = check_memory(status)
         save_info_about_request(status, address[0])
-        return render_template('/app.html', status=status)
+        return render_template('/app.html', status=status, image=image)
     except NameError:
         return f"Address doesn't exist{NameError.name}"
